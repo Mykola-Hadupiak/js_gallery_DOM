ac43e2f1a45d01e9407bcbfac4518abacbd71d9d
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-20 17:32:27 +0200

    make build

---
+++ index.6f446986.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(r){r.preventDefault();var l=r.target.closest(".list-item__link");if(l&&e.contains(l)){var c=l.getAttribute("href");t.src=c}});//# sourceMappingURL=index.6f446986.js.map
+
+//# sourceMappingURL=index.6f446986.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const list = document.querySelector(".gallery__list");
+const gallery = document.querySelector("#largeImg");
+list.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const targetImage = e.target.closest(".list-item__link");
+    if (!targetImage || !list.contains(targetImage)) return;
+    const imageSrc = targetImage.getAttribute("href");
+    gallery.src = imageSrc;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
